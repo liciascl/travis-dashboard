e8bd508fb55d3ac0ca74b2a5926734e27a63b9b1
fogelman <ddfogelman@gmail.com>
2019-05-06 01:14:26 -0300

    Changing files locations

---
+++ scrapper/main.py
@@ -0,0 +1,34 @@
+from status import Status
+import schedule
+import time
+import serial
+
+import json
+
+with open("config.json") as file:
+    json_data = json.load(file)
+
+
+if("url" not in json_data):
+    print("Url not defined, insert in config.json")
+    quit()
+
+
+def job():
+    status.run()
+    status.print_all()
+
+
+status = Status(json_data["url"])
+status.get_groups_handler()
+status.format_link()
+
+status.run()
+status.print_all()
+
+
+schedule.every(5).seconds.do(job)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
